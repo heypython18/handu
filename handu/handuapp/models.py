d72e1d4d1a9a6f9c1af259226b27d797427b7c1c
heypython18 <316772568@qq.com>
2019-01-12 09:44:45 +0800

    首页完成

--- handu/handuapp/models.py
+++ handu/handuapp/models.py
@@ -11,3 +11,32 @@ class List5(models.Model):
     unit = models.CharField(max_length=60)
     text = models.CharField(max_length=60)
     himg = models.CharField(max_length=200)
+
+class Hot(models.Model):
+    timg = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
+    price = models.CharField(max_length=40)
+    unit = models.CharField(max_length=40)
+    wimg = models.CharField(max_length=40)
+    sold = models.CharField(max_length=40)
+
+class List2(models.Model):
+
+    price = models.CharField(max_length=60)
+    unit = models.CharField(max_length=60)
+    text = models.CharField(max_length=60)
+    himg = models.CharField(max_length=200)
+
+class List3(models.Model):
+
+    price = models.CharField(max_length=60)
+    unit = models.CharField(max_length=60)
+    text = models.CharField(max_length=60)
+    himg = models.CharField(max_length=200)
+
+class List4(models.Model):
+
+    price = models.CharField(max_length=60)
+    unit = models.CharField(max_length=60)
+    text = models.CharField(max_length=60)
+    himg = models.CharField(max_length=200)
--- handu/handuapp/views.py
+++ handu/handuapp/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-from handuapp.models import HeadImg, List5
+from handuapp.models import HeadImg, List5, Hot, List2, List3, List4
 
 
 def index(request):
 
     headimg =HeadImg.objects.all()
     list5 = List5.objects.all()
+    hot = Hot.objects.all()
+    list2 = List2.objects.all()
+    list3 = List3.objects.all()
+    list4 = List4.objects.all()
 
-    return render(request,'handu Group.html',context={'headimg':headimg,'list5':list5})
+    data = {
+        'headimg':headimg,
+        'list5':list5,
+        'hot':hot,
+        'list2':list2,
+        'list2': list3,
+        'list2': list4,
+    }
+
+    return render(request,'handu Group.html',context=data)
